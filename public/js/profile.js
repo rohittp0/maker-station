cf519428462a26d6a63b1072139e9f29656946df
Rohit T P <tprohit9@gmail.com>
2023-04-23 00:26:14 +0530

    data collection done

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,61 @@
 const auth = firebase.auth();
 
-auth.onAuthStateChanged((user) => {
-    if(!user)
-        window.location.href = "/";
+const phoneTextBox = document.getElementById("phone");
+const genderTextBox = document.getElementById("gender");
+const graduationYearTextBox = document.getElementById("graduationYear");
+const venueTextBox = document.getElementById("venue");
+
+let data = {};
+
+const user = new Promise((resolve, reject) => {
+    auth.onAuthStateChanged((user) => {
+        if (!user) {
+            reject("User not logged in");
+            window.location.href = "/";
+        } else
+            resolve(user);
+    });
 });
+
+
+function updateFields(){
+    const {phno, gender, graduation_year, venue} = data;
+
+    console.log(data);
+
+    phoneTextBox.value = phno;
+    genderTextBox.value = gender;
+    graduationYearTextBox.value = graduation_year;
+    venueTextBox.value = venue;
+
+}
+
+
+async function getProfile() {
+    const uid = (await user).uid;
+
+    const db = firebase.firestore();
+    const docRef = db.collection("users").doc(uid);
+    const doc = await docRef.get().catch(console.error);
+
+    data = {
+        name: (await user).displayName,
+        email: (await user).email,
+        photoURL: (await user).photoURL,
+        uid: (await user).uid
+    };
+
+    if (doc?.exists)
+        data = {...data, ...doc.data()};
+
+    if(localStorage.getItem("phone"))
+        data.phno = localStorage.getItem("phone");
+
+    if(localStorage.getItem("gender"))
+        data.gender = localStorage.getItem("gender");
+
+    updateFields();
+}
+
+
+getProfile().catch(console.error);
