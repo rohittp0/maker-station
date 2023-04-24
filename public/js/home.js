b21f894dd4954f529a994baf4086852aa3df3b2b
Rohit T P <tprohit9@gmail.com>
2023-04-25 01:53:52 +0530

    unsubscribe

--- public/js/home.js
+++ public/js/home.js
@@ -34,11 +34,18 @@ looks like you are enjoying the maker station 😍.
 We would love to hear your feedback. 
 Would you like to fill a feedback form?`;
 
-    db.doc(`users/${uid}`).onSnapshot((doc) => {
+    const unsubscribe = db.doc(`users/${uid}`).onSnapshot((doc) => {
         const data = doc.data();
+
+        if (data.visited > 10)
+            unsubscribe();
+
         if (data.visited > 10 && !localStorage.getItem("feedback") && window.confirm(message)) {
             localStorage.setItem("feedback", "true");
             window.location.href = `https://airtable.com/shrX6eup092tsqub7?prefill_User+ID=${uid}`;
+        }
+        else if (data.visited > 10 && !localStorage.getItem("feedback")) {
+            localStorage.setItem("feedback", "false");
         }
     });
 }
